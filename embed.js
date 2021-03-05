fd5b9a12e6b2b131b721f6e4c97493a4f54bd3ff
altShiftDev <24980556+altShiftDev@users.noreply.github.com>
2021-03-04 19:24:37 -0500

    feat: improve environment support and forcing of light + dark themes

--- embed.js
+++ embed.js
@@ -1,7 +1,10 @@
 // es6 version of code, before exporting, run with babel here for ie11 compatibility
 // https://babeljs.io/repl#?browsers=defaults&build=&builtIns=false&spec=false&loose=false&code_lz=MYewdgzgLgBM8FMYF4YG8BQMYH0AmCARgK4DmAXDAGYCGANhAgDRa50g0F6W0POs5g9OgCEawANaUAFAEoUAPhjEwBKgEswCPC2w5wAQUIgATlBnzkSlWs3bduAA4mQGugkoByTw83rzMNIIdJZKmNjY6lSB8MAIAHQ47Jza8iYIUMQmYHDgECDu8QDuNNnSngDCVQCilCLsktowAMwV1TAQwCbqjrBULgC2cOk0UJqkMAOmSMB0NBAQUeoIJjBRJjQDCBBMMHjgADqesEJ0dGtg_mvRxItgEwBEJioA8mDJeA-esgDcrNixBJJDhcFAwKDPBA_GAAehhMGcCAAbggwFAIB0uj0-oMYB9xjAaB0EKBVOD1Ft_nBEIkCCQJqhgvFSBkDFAIeoSFAEOU6WRvjAAITIVBgYhnGAAMklMCZLKgbI5XJ5nmeYDeHwFwtQnl4jE8fwi1xiNPwRDI8lJ-UKJTKlRqlGqA0ITQAyljejAAJKXMb0dQALyamhgABFzRMALIgAgwcBh5EwV0rFGrAC0YZeMAAci8ACowACqrvaD1D1REhYA4g8YAAxAAyBir3uzMAACgAlF6hwXfQ0RKmAxLOVzqdxguWs9ndZW80Y0NOjtwINPqPD9qlRQKC4c4ZfjpAAHyPQr3B8KHIGcjSGSyOStBQSKxcJnKVTalGzIA7LhX3tDBEXCRddtG-LdomkPc-VIS08ifeJ2FId8HV_Md3B8ak4hHP9D1-DAhzyE5hDESRJzoZlpyVYhuXnKBF3ANNQAGRx3G5TcjW3KCSPECR5HCI1jSg00YLgyAEKQlDPzgHiyNHEDQXABsQQAfkwiFiAQfDBKwoFTlEXiwSKTR9iKABtfTSIkABdAdsAAXypYJGHQKlIkg6CIzE60Ekk-1pMswz5NAvA4zAZTODU3Y9S0uyYEcwjIFgQxjDMcjKIVGdORolU8AXNNGJoVKoA4iIuJS0woH4tyhM8-lvIkkBkP82owqMSqgJABSmiU1T1MhbTBL3Cq0tQYzVBAcyRqgWyqUco1nKQATOI8kSvNycTCj8j9WumzrutC3rIswmLBocgijStWB13S-VFVnHK6MXYIEC2NE1w3M6NugGAwBjJBUH2YBiDeqAMuqdxQZEABPL08Gkdd8KpKgVGAMZ4y6BBRgQL1-k2Hk_oIaqdKu64Ni2MEgZB1Ewcx7GIdemnynWfHSpW8mEjxrYRFMAhVh1AAGA0apZrZ4hunVASYuYFmhpFNM8Gq2I6KBoYnHVHE4PBxkoAXHAAD2hAZSlITRdYN6EAAsEHUUhLYCABGAWBYAUmhYy8CgS3KCd13oWMfW0wgS3OEm3WYAdg2YAAVijkxSEIGhpAFpgU7T-IACZZF2AWYAzqP8_1mB48T5PU_LgX4gdgAWbOYFz5oo8jou00L4uE6TtOK6rrOfmFnSuKnTLqNozw_qDkPTK1EVfvFc5pVlCi7qyudVVed4QWnnUYoFZbBK4uqLW-xrmp2r8QFgW4CR5wPXUnya2cE6A1YBmBPE1vBtfuc3DcmE2zfrhbGA1tbb2x9s7N2MAPZe3AX7fugkEoDy5gkZ-6sVYvzitgQmCQaCOEcKiPAFRLbjnhqLWKc0LoRBRmANG6gMa4MyOkSM2wIA0BZHIVySDAjIhplKGUPC0TxDyvRPh3CUSCOETQeIlsIAVH-jAE8spxFg0kfEIqlVZDEx0rVNa9Vj5bSalJVqnYSQ2xRKFUMC5qC4jIZhARKiFyyBqu5MRNMhELjUcVLR2idHYTNHox8BjT6oULIwVY7UzBgV2PY9x9EvpcOEn4vaC9hpgAiWDdw9wvYwCUALW8jCHymmmkEZRsSaDxMEukAp-iEi2jAEYygoSVgwHSU0Lkv0fwPGmrWahtD4wlAxMFLgj8jTzW0QfU0gUyIpMmbJCQiFUSkGybk7x4zVp-NEjUxChiWqUCmRIesIAbAwAzMmVQBJLH0TsaUyRTifEAlmWcKyJS3G3MwREMZOlFqcPuYE2ppR6m7JzD-fZhzjkZgqDQMARxYBnIsQucCPjPkfIoZdEY3JcYc2kNgu5ERxqmTUZ_aoyiGzqGgKiFY5QtgLDYQgTCQhehZAQMwmlLJor0EYLi-yDhZjzEWFQaGMhNbkx2DJJ5vFdjTVCD8lx0hdymg-KkYud5shbJfKYBpMBIy3GIhKYcfgoBBBCDAF0VBphwBlvy6G4xH4HzmUKGeYoJQL1Vvg1wYqDJkRFNvVG6MwC7xFusoEmy_nbOCQFe1QyepgAqJaqI0MorggGu8vcoLUD7Lisi8qaTioOtFHPURrqEDur2t6t-vS_UBoSYfWCWztqoT2lGw6Ma40CsTRpchJMik5o6qgaamaII7mFZsCA8RRx4GIGjBRp5BTDoGKO8dk6wZXhvMq6pob1VviBd-TqE6p1NuoKYC1fKlhCErV9Lis7SgjqEdsMYYBRh0JyIoq9Irb3QE0I-8A8QV2aLXfeNVJhXyap3QQD9D6_W5BUBCaG-0QqHtWLyhYp6v3-ovZBV9N7TC2xDC-udo7sOmzAD-7o14_1VIAxuoDGrt0_kIyGUA0GTCwYPWaxDrb1BnqfY_KkcJfHBvWnvS9-HEJQomHh6987RP3BIxSVde9ZU1oajaAFIGfxzHuMQWlQEEBUBWKiOIcGuC7DUDQcU96JhQE6aiNMtwvgVJExphkb9UTwPOgtPgMqypBtpOtUN9bpJOcwo5sTX1kX-Z2UW1w6loZuuiCJ24CAADSCBobGFKHgdDgQEuMBS2ljgJhQplpsLpuwoVFFRfi5J0diW8vpcK3mt-xgnxQqrfvHz_ij6hrqWp5QuXUv1dCoiPT6QaFICbSZ3TZm6AWfBCAE6HLO2CRy8lgbBWivUEWwOjzLkFP8d8wE-CQTNW1bWxlxNK26vrbC5Q2VImZEADYUAzxKxoLQ5XTyVYRNV6REAnvaiayAFraGvOKd0V1o7_y7Rn2BcAv7OmRsGfG8BEKk3aDmYJFZhbfAHM_ce2CGK22IjfL2xMjZfnIdhs1Y94LeO_s3aNHx5Fg7EkCcO5tXyOyYexpPQKgkxlsn4fILTkVX1SZkMpiAYGoNwaQxpjDOG5QpZIYgHLBW6GObxAgCYYAYIAAG9soCOAgOQOEAASNANaYAqTfirtXq4CBIk8DALwdv5Z0vsvEZo2FmIwgtyF-49kYQqUXWjZAFuDP_ULJ2L00gROh6qvZSUYGoDh7QCJlPn6_VJ_o2ANPInc9J9O_ljL-efvF8G0nx7Ze32PaTxeBAafzy4XcPZPXA5HL2T-Hx-MHwMDSFYBwqwXnSbBEl9LtxABHTSzHkzuDRjRzo3RehmUkWmF6oMPrWUfqTNUGoQS3Sog9Uee-N6cC3rPZ1_Cl5H-yif9emp5AA91Ittz-3Ou1oi-G1qp-PiJt_yCFltIAAZwKsg8thAakagzvFBgLIHIH8EAA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=true&timeTravel=false&sourceType=script&lineWrap=false&presets=env%2Ces2015%2Cenv&prettier=false&targets=&version=7.10.5&externalPlugins=babel-plugin-minify-simplify%400.5.1
 const ccce = {
-  _debug: false,
+  _env: undefined,
+  _debug: false, // deprecated
+  _verbose: false,
+  _theme: 'auto',
   _loaded: false,
   _callBack: () => undefined,
   _onAbort: () => undefined,
@@ -23,33 +26,55 @@ const ccce = {
     return (rv > 9);
   },
   _setStyle: (el, val) => { for (var x in val) el.style[x] = val[x]; },
+  _setEnvironment: (el) => {
+    if (ccce._debug) ccce._env = 'dev';
+    const _unvalidatedENV = el.getAttribute('env');
+    if (!_unvalidatedENV || !_unvalidatedENV.trim()) return console.error('CCCE: No environment flag set - use `env="stage"` or `env="prod"`');
+    if (_unvalidatedENV === 'dev' || _unvalidatedENV === 'stage' || _unvalidatedENV === 'prod') ccce._env = _unvalidatedENV;
+    else return console.error('CCCE: The value for `env` is invalid, use `env="stage"` or `env="prod"`, found:', _unvalidatedENV);
+  },
   init: (el) => {
     if (ccce._loaded) return console.warn('CCCE: Blocked 3CE script from creating more classifier iframes, don\'t call init if using "runOnload"');
     ccce._loaded = true; // prevents script from loading a second time
+    
     ccce._debug = el.getAttribute('debug') !== null && el.getAttribute('runOnload') !== 'false';
-    if (ccce._debug) console.warn('CCCE: Embed Script Initialized in Debug Mode on Dev Server - DO NOT USE "DEBUG" FLAG IN PROD!');
-    if (ccce._debug && ccce._is_ie()) console.log('CCCE: Detected IE10 or 11');
+    if (ccce._debug) console.error('CCCE: *IMPORTANT* The "debug" flag is now deprecated and will be remove in future updates. Use env="stage" & env="prod" instead to control environment');
+    
+    ccce._setEnvironment(el);
+    if (!ccce._env) return console.error('CCCE: Fatal error, unknown env value provided')
+
+    ccce._verbose = (el.getAttribute('debug') !== null || el.getAttribute('verbose') !== null) && el.getAttribute('runOnload') !== 'false';
+    if (ccce._verbose && ccce._env === 'prod') {
+      console.error('CCCE: Found verbose flag in prod, disabling logging.');
+      ccce._verbose = false
+    }
+    if (ccce._verbose) console.warn('CCCE: Embed script initialized in verbose mode');
+    if (ccce._verbose && ccce._is_ie()) console.log('CCCE: Detected IE10 or 11');
+
+    ccce._theme = el.getAttribute('force-theme')
+    if (ccce._theme !== 'light' && ccce._theme !== 'dark' && ccce._theme !== 'auto') ccce._theme = 'auto'
+    if (ccce._verbose) console.log('CCCE: Theme mode:', ccce._theme)
 
     ccce._profile = el.getAttribute('data-profile-id');
     if (!ccce._profile || !ccce._profile.trim()) return console.error('CCCE: No Profile ID provided')
-    if (ccce._debug) console.log('CCCE: Profile', ccce._profile);
+    if (ccce._verbose) console.log('CCCE: Profile', ccce._profile);
 
     const callBack = el.getAttribute('data-on-complete');
     if (callBack) {
-      if (ccce._debug) console.log('CCCE: callBack provided onLoad?', true);
+      if (ccce._verbose) console.log('CCCE: callBack provided onLoad?', true);
       ccce._callBack = window[callBack];
     }
     else {
-      if (ccce._debug) console.log('CCCE: callBack provided onLoad?', false);
+      if (ccce._verbose) console.log('CCCE: callBack provided onLoad?', false);
     }
 
     const onAbort = el.getAttribute('data-on-abort');
     if (onAbort) {
-      if (ccce._debug) console.log('CCCE: onAbort provided onLoad?', true);
+      if (ccce._verbose) console.log('CCCE: onAbort provided onLoad?', true);
       ccce._onAbort = window[onAbort];
     }
     else {
-      if (ccce._debug) console.log('CCCE: onAbort provided onLoad?', false);
+      if (ccce._verbose) console.log('CCCE: onAbort provided onLoad?', false);
     }
 
     const id = el.getAttribute('data-element-id');
@@ -69,7 +94,7 @@ const ccce = {
         position: 'relative',
       };
       if (el.getAttribute('no-shadow') !== null && el.getAttribute('runOnload') !== 'false') {
-        if (ccce._debug) console.log('CCCE: Not using Box-Shadow');
+        if (ccce._verbose) console.log('CCCE: Not using Box-Shadow');
         delete styles.boxShadow
       }
       ccce._setStyle(iframe, styles);
@@ -78,14 +103,14 @@ const ccce = {
 
     function captureMessage() {
       if (event && event.data && (event.data.hsCode || event.data.abort)) {
-        if (ccce._debug) console.log('CCCE: Received Data from iframe', event.data)
+        if (ccce._verbose) console.log('CCCE: Received Data from iframe', event.data)
         if (event.data.abort) {
-          if (ccce._debug) console.log('CCCE: User Aborted', event.data);
+          if (ccce._verbose) console.log('CCCE: User Aborted', event.data);
           if (ccce._onAbort && ccce._onAbort.length > 0) return ccce._onAbort(event.data);
           return console.warn('CCCE: User Aborted but no "onAbort" function was provided');
         }
         if (ccce._callBack && ccce._callBack.length > 0) {
-          if (ccce._debug) console.log('CCCE: callBack Found - Sending Data', event.data)
+          if (ccce._verbose) console.log('CCCE: callBack found - sending data', event.data)
           ccce._callBack(event.data);
         }
         else {
@@ -99,18 +124,18 @@ const ccce = {
   classify: (params, callBack, onAbort) => {
     if (!ccce._loaded) return console.error('CCCE: Must call ccce.init(el) before classifying');
     if (callBack !== null && typeof callBack === 'function') {
-      if (ccce._debug) console.log('CCCE: callBack provided onClassify?', true);
+      if (ccce._verbose) console.log('CCCE: callBack provided onClassify?', true);
       ccce._callBack = callBack;
     }
     if (onAbort !== null && typeof onAbort === 'function') {
-      if (ccce._debug) console.log('CCCE: onAbort provided onClassify?', true);
+      if (ccce._verbose) console.log('CCCE: onAbort provided onClassify?', true);
       ccce._onAbort = onAbort;
     }
     if (params.hs6 === undefined || typeof params.hs6 !== 'boolean') {
-      if (ccce._debug) console.warn('CCCE: No hs6 preference provided, defaulting to', false);
+      if (ccce._verbose) console.warn('CCCE: No hs6 preference provided, defaulting to', false);
       params.hs6 = false;
     } else {
-      if (ccce._debug) console.log('CCCE: hs6', params.hs6);
+      if (ccce._verbose) console.log('CCCE: hs6', params.hs6);
     }
 
     if (!params.product || !params.product.trim()) return console.error('CCCE: No product provided for classification');
@@ -119,24 +144,24 @@ const ccce = {
       if (!params.origin || !params.origin.trim()) return console.error('CCCE: No origin country provided for classification');
     }
     if (!params.lang || !params.lang.trim()) {
-      if (ccce._debug) console.warn('CCCE: No language preference provided, defaulting to "en-us"');
+      if (ccce._verbose) console.warn('CCCE: No language preference provided, defaulting to "en-us"');
       params.lang = 'en';
     }
     else {
-      if (ccce._debug) console.log('CCCE: lang', params.lang);
+      if (ccce._verbose) console.log('CCCE: lang', params.lang);
     }
 
     if (params.useKeyboard === undefined || typeof params.useKeyboard !== 'boolean') {
-      if (ccce._debug) console.warn('CCCE: No useKeyboard preference provided, defaulting to', false);
+      if (ccce._verbose) console.warn('CCCE: No useKeyboard preference provided, defaulting to', false);
       params.useKeyboard = false;
     }
     else {
-      if (ccce._debug) console.log('CCCE: useKeyboard?', params.useKeyboard);
+      if (ccce._verbose) console.log('CCCE: useKeyboard?', params.useKeyboard);
     }
     
-    if (ccce._debug) console.log('CCCE: Classifying with params:', params);
+    if (ccce._verbose) console.log('CCCE: Classifying with params:', params);
     const iframe = document.getElementById('ccce-classyvue');
-    iframe.src = `https://${ccce._debug ? 'classyvue-dev' : 'classyvue'}.3ce.com/${params.lang}/?product=${encodeURI(params.product)}&dest=${!params.hs6 ? params.destination : ''}&origin=${!params.hs6 ? params.origin : ''}&useKeyboard=${params.useKeyboard}&hs6=${params.hs6}&profile=${ccce._profile}${ccce._is_ie() ? `&ie11_domain=${document.location.origin}` : ''}&debug=${ccce._debug === true}`;
+    iframe.src = `https://${ccce._env === 'prod' ? 'classyvue' : 'classyvue-' + ccce._env}.3ce.com/${params.lang}/?product=${encodeURI(params.product)}&dest=${!params.hs6 ? params.destination : ''}&origin=${!params.hs6 ? params.origin : ''}&useKeyboard=${params.useKeyboard}&hs6=${params.hs6}&profile=${ccce._profile}${ccce._is_ie() ? `&ie11_domain=${document.location.origin}` : ''}&debug=${ccce._env !== 'prod'}&force_theme=${ccce._theme}`;
   }
 };
 // on load
@@ -144,7 +169,7 @@ const ccce = {
   () => {
     const el = document.querySelector('script[data-element-id]');
     const runOnload = el.getAttribute('runOnload') !== null && el.getAttribute('runOnload') !== 'false';
-    if (ccce._debug) console.log('CCCE: runOnload?', runOnload);
+    if (ccce._verbose) console.log('CCCE: runOnload?', runOnload);
     if (runOnload) {
       ccce.init(el);
     }
